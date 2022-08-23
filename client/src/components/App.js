24d3602b4e2d117213ec98201c4a0e9d5638157a
NicMortelliti <npmortelliti@gmail.com>
2022-08-23 16:07:54 -0700

    Set up basic routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,19 @@
+import { Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Route exact path="/">
+        "home"
+      </Route>
+      <Route exact path="/signup">
+        "signup"
+      </Route>
+      <Route exact path="/new_appointment">
+        "new appointment"
+      </Route>
+    </div>
+  );
 }
 
 export default App;
