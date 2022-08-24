3f4e408573fa8d93411e059acd1cd24042d67a8d
NicMortelliti <npmortelliti@gmail.com>
2022-08-24 16:24:43 -0700

    Add global theming

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../style/GlobalStyles";
+import { lightTheme, darkTheme } from "../style/Themes";
 import { Route, Switch } from "react-router-dom";
 
 // Import components
 import Navbar from "./Navbar";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
-    <div className="App">
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          "home"
-        </Route>
-        <Route exact path="/signup">
-          "signup"
-        </Route>
-        <Route exact path="/new_appointment">
-          "new appointment"
-        </Route>
-      </Switch>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <div className="App">
+          <Navbar />
+          <button onClick={themeToggler}>Switch Theme</button>
+          <Switch>
+            <Route exact path="/">
+              "home"
+            </Route>
+            <Route exact path="/signup">
+              "signup"
+            </Route>
+            <Route exact path="/new_appointment">
+              "new appointment"
+            </Route>
+          </Switch>
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/style/Themes.js
@@ -0,0 +1,15 @@
+// Light Theme
+export const lightTheme = {
+  body: "#1b2232",
+  text: "#d7ccb1",
+  toggleBorder: "#1b2232",
+  background: "#c49bbb",
+};
+
+// Dark Theme
+export const darkTheme = {
+  body: "#1C1231",
+  text: "#FF019A",
+  toggleBorder: "#9B00E8",
+  background: "#0161E8",
+};
