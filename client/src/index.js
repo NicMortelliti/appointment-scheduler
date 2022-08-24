566ea1cd0c40aa08eaa631a8a93a759540f3d298
NicMortelliti <npmortelliti@gmail.com>
2022-08-24 16:24:32 -0700

    Update css import path

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./styles.css"
+import "./style/styles.css";
 
 // Components
 import App from "./components/App";
