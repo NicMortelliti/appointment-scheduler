846b1cb8e58e6e2a25be3357829975ee16fac873
Nic Mortelliti <npmortelliti@gmail.com>
2022-08-27 09:00:57 -0700

    Remove component templates

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button, Error, Input, FormField, Label, Textarea } from "../style";
 
 function Signup({ onLogin }) {
   const [username, setUsername] = useState("");
@@ -38,62 +37,50 @@ function Signup({ onLogin }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <FormField>
-        <Label htmlFor="username">Username</Label>
-        <Input
+      <form>
+        <label htmlFor="username">Username</label>
+        <input
           type="text"
           id="username"
           autoComplete="off"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-      </FormField>
-      <FormField>
-        <Label htmlFor="password">Password</Label>
-        <Input
+        <label htmlFor="password">Password</label>
+        <input
           type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           autoComplete="current-password"
         />
-      </FormField>
-      <FormField>
-        <Label htmlFor="password">Password Confirmation</Label>
-        <Input
+        <label htmlFor="password">Password Confirmation</label>
+        <input
           type="password"
           id="password_confirmation"
           value={passwordConfirmation}
           onChange={(e) => setPasswordConfirmation(e.target.value)}
           autoComplete="current-password"
         />
-      </FormField>
-      <FormField>
-        <Label htmlFor="imageUrl">Profile Image</Label>
-        <Input
+        <label htmlFor="imageUrl">Profile Image</label>
+        <input
           type="text"
           id="imageUrl"
           value={imageUrl}
           onChange={(e) => setImageUrl(e.target.value)}
         />
-      </FormField>
-      <FormField>
-        <Label htmlFor="bio">Bio</Label>
-        <Textarea
+        <label htmlFor="bio">Bio</label>
+        <textarea
           rows="3"
           id="bio"
           value={bio}
           onChange={(e) => setBio(e.target.value)}
         />
-      </FormField>
-      <FormField>
-        <Button type="submit">{isLoading ? "Loading..." : "Sign Up"}</Button>
-      </FormField>
-      <FormField>
-        {errors.map((err) => (
+        <button type="submit">{isLoading ? "Loading..." : "Sign Up"}</button>
+        {/* {errors.map((err) => (
           <Error key={err}>{err}</Error>
-        ))}
-      </FormField>
+        ))} */}
+      </form>
     </form>
   );
 }
