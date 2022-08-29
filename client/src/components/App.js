7b7bf5942740a6e64c0b93e1844bcffd3db7116f
Nic Mortelliti <npmortelliti@gmail.com>
2022-08-29 14:11:11 -0700

    Add overflow hidden to nav bar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -21,21 +21,23 @@ function App() {
       <GlobalStyles />
       <div className="App">
         <Navbar toggler={themeToggler} />
-        <Switch>
-          <Route exact path="/">
-            "home"
-          </Route>
-          <Route exact path="/signup">
-            {signUp ? (
-              <Signup setSignUp={setSignUp} />
-            ) : (
-              <Login setSignUp={setSignUp} />
-            )}
-          </Route>
-          <Route exact path="/new_appointment">
-            "new appointment"
-          </Route>
-        </Switch>
+        <div className="main">
+          <Switch>
+            <Route exact path="/">
+              "home"
+            </Route>
+            <Route exact path="/signup">
+              {signUp ? (
+                <Signup setSignUp={setSignUp} />
+              ) : (
+                <Login setSignUp={setSignUp} />
+              )}
+            </Route>
+            <Route exact path="/new_appointment">
+              "new appointment"
+            </Route>
+          </Switch>
+        </div>
       </div>
     </ThemeProvider>
   );
