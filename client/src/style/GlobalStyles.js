8e06b9fb3231a233b1a18402f423eb482750827f
NicMortelliti <npmortelliti@gmail.com>
2022-08-29 15:59:47 -0700

    Change font color

--- client/src/style/GlobalStyles.js
+++ client/src/style/GlobalStyles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.sixty};
-    color: ${({ theme }) => theme.ten};
+    color: ${({ theme }) => theme.thirty};
     font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
     transition: all 0.0s linear;
   }
