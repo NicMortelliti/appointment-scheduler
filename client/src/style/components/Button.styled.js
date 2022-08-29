182149de2fb2c429b96e5f8814563dcb84ef108f
Nic Mortelliti <npmortelliti@gmail.com>
2022-08-29 14:40:26 -0700

    Adjust component color

--- client/src/style/components/Button.styled.js
+++ client/src/style/components/Button.styled.js
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  background: ${(props) => (props.primary ? props.theme.ten : "none")};
-  color: ${(props) => (props.primary ? props.theme.sixty : props.theme.ten)};
+  background: ${(props) => (props.primary ? props.theme.thirty : "none")};
+  color: ${(props) => (props.primary ? props.theme.sixty : props.theme.thirty)};
 
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: ${(props) => (props.primary ? props.theme.ten : "none")};
-  border-radius: 3px;
+  border-color: ${(props) => (props.primary ? props.theme.ten : "none")};
+  border-style: none;
+  box-shadow: ${(props) => (props.primary ? "2px 2px 2px black" : "none")};
+  border-radius: 0px;
   cursor: pointer;
 `;
 
