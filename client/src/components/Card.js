ffae80cf6006212430b25a63e68516aa16e8a8d6
NicMortelliti <npmortelliti@gmail.com>
2022-09-01 17:26:07 -0700

    Update spacing/positioning of card components

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,26 +2,28 @@ import React from "react";
 
 function Card() {
   return (
-    <div className="card">
-      <div className="date">
+    <div className="card-grid-container">
+      <div className="card-date">
         <p className="month">SEP</p>
         <h1 className="day">10</h1>
         <p className="time">12:30 PM</p>
       </div>
-      <div className="details">
-        <div className="doctor-details">
+      <div className="card-details">
+        <div className="details card-doctor">
           <h5 className="label">Doctor:</h5>
           <p className="doctor-name">Dr. Firstname Lastname</p>
         </div>
-        <div className="location-details">
-          <h6 className="label">Location:</h6>
+        <div className="details card-location">
+          <h5 className="label">Location:</h5>
           <p className="address-street">15270 SW Holly Hill Rd</p>
           <p className="address-town-state">Hillsboro, OR 97123</p>
         </div>
       </div>
-      <div className="card-btn-group">
-        <button className="card-btn">Reschedule</button>
-        <button className="card-btn cancel">Cancel</button>
+      <div className="card-reschedule-btn">
+        <button className="primary">Reschedule</button>
+      </div>
+      <div className="card-cancel-btn">
+        <button className="secondary">Cancel Appointment</button>
       </div>
     </div>
   );
