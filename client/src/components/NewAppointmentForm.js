e07a259d19cc5a7b9dd2b67991f6c3eecb4aa5c1
NicMortelliti <npmortelliti@gmail.com>
2022-09-03 15:53:09 -0700

    Add doctor picker with test data

--- client/src/components/NewAppointmentForm.js
+++ client/src/components/NewAppointmentForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Select from "react-select";
 
 function NewAppointmentForm() {
   const [formData, setFormData] = useState({
@@ -8,15 +9,37 @@ function NewAppointmentForm() {
     doctor: "",
   });
 
+  const doctors = [
+    { value: "1", label: "Jean-Martin Charcot" },
+    { value: "2", label: "John Down" },
+  ];
+
   function handleSubmit(e) {
     e.preventDefault(e);
     console.log("Submitting form");
   }
 
+  const ApptSelectField = ({ name, options }) => {
+    console.log(options);
+    return (
+      <Select
+        className="basic-single"
+        defaultValue={null}
+        isDisabled={false}
+        isLoading={false}
+        isSearchable={true}
+        name={name}
+        options={options}
+      />
+    );
+  };
+
   return (
     <form className="center" onSubmit={handleSubmit}>
-      <label htmlFor="doctor">Doctor</label>
-      <label htmlFor="location">Location</label>
+      <div>
+        <label htmlFor="selectDoctor">Doctor</label>
+        <Select name="selectDoctor" options={doctors} />
+      </div>
       <label htmlFor="date">Date</label>
       <label htmlFor="time">Time</label>
     </form>
