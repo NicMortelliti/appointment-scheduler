9876e3d44bc8c3202d902fbee96ce245a8b3b8e6
Nic Mortelliti <npmortelliti@gmail.com>
2022-09-09 07:14:28 -0700

    Delete styled components

--- client/src/style/StyledCenterDiv.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const StyledCenterDiv = styled.div`
-  display: block;
-  margin: 0 auto;
-  width: 100%;
-`;
-
-export default StyledCenterDiv;
--- client/src/style/StyledDateSelectField.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-import DatePicker from "react-datepicker";
-
-const StyledDateSelectField = styled(DatePicker)`
-  margin-bottom: 2rem;
-`;
-export default StyledDateSelectField;
