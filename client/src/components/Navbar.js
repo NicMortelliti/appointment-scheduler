d2f81d2871d7dd68b46ab56fa48631035a4b4996
NicMortelliti <npmortelliti@gmail.com>
2022-09-12 15:37:27 -0700

    Add theme

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,6 +3,11 @@ import { NavLink } from "react-router-dom";
 import { Button, Navbar as Nav } from "@blueprintjs/core";
 
 function Navbar({ user, setUser }) {
+  const theme = {
+    fontSize: "18px",
+    color: "#5C255C",
+  };
+
   function handleLogoutClick() {
     fetch("/logout", { method: "DELETE" }).then((r) => {
       if (r.ok) {
@@ -11,19 +16,40 @@ function Navbar({ user, setUser }) {
     });
   }
   return (
-    <Nav fixedToTop>
+    <Nav
+      fixedToTop
+      style={{
+        background: "#8BCDBC",
+      }}>
       <Nav.Group>
-        <Nav.Heading>PacNW Health</Nav.Heading>
+        <Nav.Heading
+          style={{
+            color: "#5c255c",
+            fontWeight: "bold",
+            fontFamily: "Poiret One",
+          }}>
+          PacNW Health
+        </Nav.Heading>
         <Nav.Divider />
         <NavLink to="/" activeClassName="active" exact>
-          <Button minimal icon="home" text="Home" />
+          <Button minimal icon="home" text="Home" style={theme} />
         </NavLink>
         <NavLink to="/new_appointment" activeClassName="active">
-          <Button minimal icon="calendar" text="New Appointment" />
+          <Button
+            minimal
+            icon="calendar"
+            text="New Appointment"
+            style={theme}
+          />
         </NavLink>
       </Nav.Group>
       <Nav.Group align="right">
-        <Button minimal onClick={handleLogoutClick} text="Logout" />
+        <Button
+          minimal
+          onClick={handleLogoutClick}
+          text="Logout"
+          style={theme}
+        />
       </Nav.Group>
     </Nav>
   );
