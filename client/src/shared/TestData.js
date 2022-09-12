f85bcc1fea68c3db20449bb224b511b1d2f7f414
Nic Mortelliti <npmortelliti@gmail.com>
2022-09-12 10:17:53 -0700

    Add test data

---
+++ client/src/shared/TestData.js
@@ -0,0 +1,26 @@
+export const doctors = [
+  { value: "1", label: "Jean-Martin Charcot" },
+  { value: "2", label: "John Down" },
+];
+
+export const locations = [
+  { value: "1", label: "Lake Oswego" },
+  { value: "2", label: "Wilsonville" },
+  { value: "3", label: "Tigard" },
+  { value: "4", label: "Portland" },
+];
+
+export const dates = [
+  new Date("09/12/2022"),
+  new Date("09/13/2022"),
+  new Date("09/14/2022"),
+  new Date("09/15/2022"),
+  new Date("09/16/2022"),
+];
+
+export const times = [
+  { value: "1", label: "11:00 AM" },
+  { value: "2", label: "11:30 AM" },
+  { value: "3", label: "2:00 PM" },
+  { value: "4", label: "2:30 PM" },
+];
