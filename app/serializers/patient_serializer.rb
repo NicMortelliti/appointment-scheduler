a85d56dc5eff23e5228cc3c0297763f179be03f7
Nic Mortelliti <npmortelliti@gmail.com>
2022-09-16 10:43:51 -0700

    Generate base patient model

---
+++ app/serializers/patient_serializer.rb
@@ -0,0 +1,3 @@
+class PatientSerializer < ActiveModel::Serializer
+  attributes :id, :first_name, :last_name, :email, :password_digest
+end
---
+++ db/migrate/20220916174206_create_patients.rb
@@ -0,0 +1,12 @@
+class CreatePatients < ActiveRecord::Migration[6.1]
+  def change
+    create_table :patients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
