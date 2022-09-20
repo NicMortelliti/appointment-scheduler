dc34bfa5da68ab71476aca76fe3177d454f5a70c
Nic Mortelliti <npmortelliti@gmail.com>
2022-09-20 11:22:13 -0700

    Fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
   private
 
   def authorize
-    @current_user = User.find_by(id: session[:user_id])
+    @current_user = Patient.find_by(id: session[:user_id])
 
     render json: { errors: ['Not authorized'] }, status: :unauthorized unless @current_user
   end
@@ -16,5 +16,4 @@ class ApplicationController < ActionController::API
   def render_unprocessable_entity_response(exception)
     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
-
 end
