0b4aee3fc763f374238df33455c07c16c596c3d0
Nic Mortelliti <npmortelliti@gmail.com>
2022-09-20 17:10:48 -0700

    Update column names for start and end date/time

--- db/migrate/20220916175849_create_appointments.rb
+++ db/migrate/20220916175849_create_appointments.rb
@@ -1,7 +1,8 @@
 class CreateAppointments < ActiveRecord::Migration[6.1]
   def change
     create_table :appointments do |t|
-      t.datetime :slot
+      t.datetime :start
+      t.datetime :end
       t.integer :patient_id
       t.integer :doctor_id
 
