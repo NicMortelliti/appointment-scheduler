c355bc18b41dbde088120286ca9c9f382f7e8f62
Nic Mortelliti <npmortelliti@gmail.com>
2022-10-05 11:33:11 -0700

    Remove routing resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :doctors
-  resources :patients
+  # resources :appointments
+  # resources :doctors
+  # resources :patients
 
   delete '/appoointments', to: 'appointments#destroy'
   delete '/logout', to: 'sessions#destroy'
